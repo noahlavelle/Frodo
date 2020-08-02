b3b418a98091f53f8ff94fdb74a69adfaa343602
NoahLavelle <noah.lavelle@gmail.com>
2020-08-02 17:59:53 +0100

    Added werewolves game party system and roles assigning

--- commands/games/wearwolves.js
+++ commands/games/wearwolves.js
@@ -2,20 +2,19 @@ const utils = require('../../utils');
 const { color } = require('jimp');
 
 module.exports = {
-	name: 'wearwolves',
-    description: 'A game of wearwolves',
+	name: 'werewolves',
+    description: 'A game of Werewolves',
 	execute(message, args, client) {
-        const uniqueRoles = ['wearwolf', 'seer', 'doctor', 'witch', 'drunk', 'alpha'];
         class Game {
             constructor() {
-                this.uniqueRoles = ['wearwolf', 'seer', 'doctor', 'witch', 'drunk', 'alpha'];
+                this.uniqueRoles = ['werewolf', 'seer'];
                 this.players = [];
+                this.checker = (arr, target) => target.every(v => arr.includes(v));
                 this.init();
             }
 
             async init() {
-                this.players.push(message.author.id)
-                this.msg = await message.channel.send(`<@${message.author.id}> has started a game of **wearwolves**! Press the blue join button to join the party. Once there are 7 players <@${message.author.id}> will have the option to start the game.`)
+                this.msg = await message.channel.send(`<@${message.author.id}> has started a game of **werewolves**! Press the blue join button to join the party. Once there are 7 players <@${message.author.id}> will have the option to start the game.`)
                 this.msg.react('🔵');
                 this.reactionCount = 0;
                 this.party();
@@ -41,17 +40,27 @@ module.exports = {
                     return reaction.emoji.name == '🟢' && user.id == message.author.id;
                 }
                 await this.getReaction(filter, 600000)
-                const keys = await this.reaction.users.cache.keys();
-                for (let i in keys) {
-                    console.log('hi')
-                    console.log(i, this.reaction.users.cache[keys[i]])
-                    if (!this.reaction.users.cache[keys[i]].bot) {
-                        console.log(this.reaction.users.cache[keys[i]].id)
-                        this.players.push(this.reaction.users.cache[keys[i]].id);
+                const values = Array.from(this.reaction.users.cache.values());
+                for (const value in values) {
+                    if (!values[value].bot) {
+                        this.players[value] = new Array(1).fill(values[value].id);
                     }
                 }
 
-                
+                this.assiginedRoles = [];
+                this.players[0] = new Array(1).fill(message.author.id);
+                for (const player in this.players) {
+                    await this.assignRole(player);
+                }
+                console.log(this.players)
+            }
+
+            assignRole(player) {
+                const roleNumber = Math.round(Math.random() * (this.uniqueRoles.length - 1));
+                if (this.checker(this.assiginedRoles, this.uniqueRoles)) return this.players[player].push('villager')
+                if (this.assiginedRoles.includes(this.uniqueRoles[roleNumber])) return this.assignRole(player);
+                this.players[player].push(this.uniqueRoles[roleNumber])
+                return this.assiginedRoles.push(this.uniqueRoles[roleNumber]);
             }
 
             async getReaction(filter, time) {
