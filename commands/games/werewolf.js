12ae44714029a56482a4296c6c1840d7ca580370
unknown <noah.lavelle@gmail.com>
2021-08-29 13:48:00 +0100

    Add base werewolf structure

--- commands/games/werewolf.js
+++ commands/games/werewolf.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Werewolf = void 0;
 const discord_js_1 = require("discord.js");
 const partyLobby_1 = require("../../partyLobby");
-const index_1 = require("../../index");
 var Roles;
 (function (Roles) {
     Roles[Roles["Seer"] = 0] = "Seer";
@@ -12,19 +10,19 @@ var Roles;
     Roles[Roles["Villager"] = 3] = "Villager";
 })(Roles || (Roles = {}));
 const selectionReactions = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹', '🇺', '🇻', '🇼', '🇽', '🇾', '🇿', '0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
-const wait = require('util').promisify(setTimeout);
 const roleStack = [Roles.Werewolf];
 class Werewolf extends partyLobby_1.PartyLobby {
     constructor(interaction) {
         super(interaction, 'Werewolf', '**Werewolf** is a classic social deduction party game. ' +
             'Press the blue button to join and the red button to leave. The minimum number of players are 2', 2);
-        this.gameRunning = true;
         this.interaction = interaction;
-        this.playerRoles = [];
-        this.werewolves = [];
     }
     async gameStarted(players) {
         super.gameStarted(players);
+        this.assignRoles(players);
+        await this.createChannels();
+    }
+    assignRoles(players) {
         this.players = players;
         for (const role of roleStack) {
             const randomIndex = Math.floor(Math.random() * players.length);
@@ -37,165 +35,18 @@ class Werewolf extends partyLobby_1.PartyLobby {
             this.playerRoles.push([user, Roles.Villager]);
         }
         this.players = players;
-        await this.createChannels();
-        while (this.gameRunning) {
-            await this.sendNightMessages();
-            const werewolvesFilter = (msg) => {
-                return msg.content.split(' ').length == 1 && msg.mentions.users.size == 1 && !this.werewolves.includes(msg.mentions.users.first());
-            };
-            const generalFilter = (msg) => {
-                return msg.content.split(' ').length == 1 && msg.mentions.users.size == 1;
-            };
-            const werewolvesVictimIndex = await this.runVictimSelection(15000, true);
-            await this.sendDayMessages(this.playerRoles[werewolvesVictimIndex][0]);
-            const villagersVictimIndex = await this.runVictimSelection(15000, false);
-            await this.generalChannel.send(`${this.playerRoles[villagersVictimIndex][0]} was successfully killed.`);
-        }
-    }
-    async runVictimSelection(voteTime, isWerewolvesVote) {
-        let victimDescription = '';
-        const identifiers = [];
-        let actualIndex = 0;
-        this.playerRoles.forEach((data) => {
-            if (!isWerewolvesVote || (!this.werewolves.includes(data[0]) && isWerewolvesVote)) {
-                victimDescription += `${selectionReactions[actualIndex]}: ${data[0].username}\n`;
-                identifiers.push(selectionReactions[actualIndex]);
-            }
-            else {
-                actualIndex -= 1;
-            }
-            actualIndex++;
-        });
-        const victimEmbed = new discord_js_1.MessageEmbed()
-            .setTitle('Chose Victim:')
-            .setColor(index_1.EmbedColor)
-            .setDescription(victimDescription + `\nYou now have ${Math.round(voteTime / 60000)} minute(s) to decide. At the end of the time the most voted player will be killed, but if it is a draw nobody will die`);
-        const victimSelectionMessage = isWerewolvesVote ? await this.werewolvesChannel.send({ embeds: [victimEmbed] }) : await this.generalChannel.send({ embeds: [victimEmbed] });
-        for (const identifier of identifiers) {
-            await victimSelectionMessage.react(identifier);
-        }
-        let selectedUserIndex = 0;
-        await sleep(voteTime);
-        let leadingReaction = ['', 0];
-        victimSelectionMessage.reactions.cache.forEach((reaction) => {
-            if (reaction.count > leadingReaction[1]) {
-                leadingReaction = [reaction.emoji.name, reaction.count];
-            }
-        });
-        selectionReactions.forEach((emoji, i) => {
-            if (leadingReaction[0] == emoji) {
-                selectedUserIndex = i;
-                return;
-            }
-        });
-        return selectedUserIndex;
-    }
-    async checkWin() {
-        let werewolfCount = 0;
-        let villagerCount = 0;
-        for (const playerRole of this.playerRoles) {
-            if (playerRole[1] == Roles.Werewolf) {
-                werewolfCount++;
-            }
-            else {
-                villagerCount++;
-            }
-        }
-        if (werewolfCount >= villagerCount) {
-            await this.endGame(true);
-        }
-        else if (werewolfCount == 0) {
-            await this.endGame(false);
-        }
-    }
-    async endGame(hasWerewolvesWon) {
-        await this.werewolvesChannel.delete();
-        await this.generalChannel.delete();
-        await this.message.channel.send(`The game of werewolf has ended. The winners are the ${hasWerewolvesWon ? 'werewolves' : 'villagers'}!`);
-        this.gameRunning = false;
-    }
-    async killPlayer(player) {
-        await this.generalChannel.permissionOverwrites.edit(player.id, { SEND_MESSAGES: false });
-        await this.werewolvesChannel.permissionOverwrites.edit(player.id, { SEND_MESSAGES: false });
-        this.players.splice(this.players.indexOf(player));
-        await this.checkWin();
-    }
-    async sendDayMessages(victim) {
-        await this.generalChannel.send({ embeds: [
-                new discord_js_1.MessageEmbed()
-                    .setTitle('Day Time:')
-                    .setDescription(`As dawn breaks you discover the dead body of ${victim}. It is your job to decide who to kill`)
-                    .setColor(index_1.EmbedColor),
-            ] });
-        await this.werewolvesChannel.send({ embeds: [
-                new discord_js_1.MessageEmbed()
-                    .setTitle('Day Time:')
-                    .setDescription(`${victim} was successfully killed, and now you need to go to the ${this.generalChannel} to convince the villagers you are innocent`)
-                    .setColor(index_1.EmbedColor),
-            ] });
-    }
-    async sendNightMessages() {
-        await this.generalChannel.send({ embeds: [
-                new discord_js_1.MessageEmbed()
-                    .setTitle('Night Time:')
-                    .setDescription('The werewolves will now decide who to kill. If you have a special role you will be DMd when you need to make a choice')
-                    .setColor(index_1.EmbedColor),
-            ] });
-        await this.werewolvesChannel.send({ embeds: [
-                new discord_js_1.MessageEmbed()
-                    .setTitle('Night Time:')
-                    .setDescription('You need to choose who to kill. When you have chosen who to kill just mention them in an empty message')
-                    .setColor(index_1.EmbedColor),
-            ] });
     }
     async createChannels() {
-        const werewolvesPermissionOverride = [];
-        const generalPermissionsOverride = [];
-        werewolvesPermissionOverride.push({
-            id: this.interaction.guild.roles.everyone,
-            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-        });
-        generalPermissionsOverride.push({
-            id: this.interaction.guild.roles.everyone,
-            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-        });
-        for (const playerRole of this.playerRoles) {
-            if (playerRole[1] == Roles.Werewolf) {
-                werewolvesPermissionOverride.push({
-                    id: playerRole[0].id,
-                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-                });
-            }
-            generalPermissionsOverride.push({
-                id: playerRole[0].id,
-                allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-            });
-        }
-        this.generalChannel = await this.interaction.guild.channels.create('Werewolf - General', {
+        this.interaction.guild.channels.create('new-channel', {
             type: 'GUILD_TEXT',
-            permissionOverwrites: generalPermissionsOverride,
+            permissionOverwrites: [
+                {
+                    id: this.interaction.guild.roles.everyone,
+                    deny: [discord_js_1.Permissions.FLAGS.VIEW_CHANNEL],
+                },
+            ],
         });
-        this.werewolvesChannel = await this.interaction.guild.channels.create('Werewolf - Werewolves', {
-            type: 'GUILD_TEXT',
-            permissionOverwrites: werewolvesPermissionOverride,
-        });
-        await this.generalChannel.send({ embeds: [
-                new discord_js_1.MessageEmbed()
-                    .setTitle('Werewolf')
-                    .setDescription('Welcome to Werewolf. This is the channel for everyone, so keep your role here secret. If you have a special role like seer or doctor you will be ' +
-                    'DMd. Werewolves have a private channel that villagers cannot see.')
-                    .setColor(index_1.EmbedColor),
-            ] });
-        await this.werewolvesChannel.send({ embeds: [
-                new discord_js_1.MessageEmbed()
-                    .setTitle('Werewolf')
-                    .setDescription('Welcome to werewolf. You are all werewolves and your goal is to kill all the villagers')
-                    .setColor(index_1.EmbedColor),
-            ] });
     }
 }
-exports.Werewolf = Werewolf;
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+exports.default = Werewolf;
 //# sourceMappingURL=werewolf.js.map
--- commands/games/werewolf.ts
+++ commands/games/werewolf.ts
@@ -1,15 +1,5 @@
-import {
-	CommandInteraction,
-	Message,
-	MessageEmbed,
-	MessageReaction,
-	ReactionEmoji,
-	Role,
-	TextChannel,
-	User,
-} from 'discord.js';
+import {CommandInteraction, Permissions, User} from 'discord.js';
 import {PartyLobby} from '../../partyLobby';
-import {EmbedColor} from '../../index';
 
 enum Roles {
 	Seer,
@@ -18,30 +8,31 @@ enum Roles {
 	Villager,
 }
 const selectionReactions = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹', '🇺', '🇻', '🇼', '🇽', '🇾', '🇿', '0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
-
-const wait = require('util').promisify(setTimeout);
 const roleStack = [Roles.Werewolf];
 
-export class Werewolf extends PartyLobby {
+export default class Werewolf extends PartyLobby {
 	interaction: CommandInteraction;
+
+	/* Player Data */
 	players: User[];
 	playerRoles: any[][];
-	generalChannel: TextChannel;
-	werewolvesChannel: TextChannel;
 	werewolves: User[];
-	gameRunning = true;
 
-	constructor(interaction) {
+	constructor(interaction: CommandInteraction) {
 		super(interaction, 'Werewolf', '**Werewolf** is a classic social deduction party game. ' +
 			'Press the blue button to join and the red button to leave. The minimum number of players are 2', 2);
 
 		this.interaction = interaction;
-		this.playerRoles = [];
-		this.werewolves = [];
 	}
 
 	async gameStarted(players: User[]) {
 		super.gameStarted(players);
+
+		this.assignRoles(players);
+		await this.createChannels();
+	}
+
+	assignRoles(players: User[]) {
 		this.players = players;
 
 		for (const role of roleStack) {
@@ -56,181 +47,17 @@ export class Werewolf extends PartyLobby {
 		}
 
 		this.players = players;
-		await this.createChannels();
-
-		while (this.gameRunning) {
-			await this.sendNightMessages();
-			const werewolvesFilter = (msg: Message) => {
-				return msg.content.split(' ').length == 1 && msg.mentions.users.size == 1 && !this.werewolves.includes(msg.mentions.users.first());
-			};
-			const generalFilter = (msg: Message) => {
-				return msg.content.split(' ').length == 1 && msg.mentions.users.size == 1;
-			};
-
-			const werewolvesVictimIndex = await this.runVictimSelection(15000, true);
-			await this.sendDayMessages(this.playerRoles[werewolvesVictimIndex][0]);
-			const villagersVictimIndex = await this.runVictimSelection(15000, false);
-			await this.generalChannel.send(`${this.playerRoles[villagersVictimIndex][0]} was successfully killed.`);
-		}
-	}
-
-	async runVictimSelection(voteTime: number, isWerewolvesVote: boolean) {
-		let victimDescription = '';
-		const identifiers = [];
-		let actualIndex = 0;
-		this.playerRoles.forEach((data) => {
-			if (!isWerewolvesVote || (!this.werewolves.includes(data[0]) && isWerewolvesVote)) {
-				victimDescription += `${selectionReactions[actualIndex]}: ${data[0].username}\n`;
-				identifiers.push(selectionReactions[actualIndex]);
-			} else {
-				actualIndex -= 1;
-			}
-
-			actualIndex ++;
-		});
-
-		const victimEmbed = new MessageEmbed()
-			.setTitle('Chose Victim:')
-			.setColor(EmbedColor)
-			.setDescription(victimDescription + `\nYou now have ${Math.round(voteTime / 60000)} minute(s) to decide. At the end of the time the most voted player will be killed, but if it is a draw nobody will die`);
-		const victimSelectionMessage = isWerewolvesVote ? await this.werewolvesChannel.send({embeds: [victimEmbed]}) : await this.generalChannel.send({embeds: [victimEmbed]});
-		for (const identifier of identifiers) {
-			await victimSelectionMessage.react(identifier);
-		}
-
-		let selectedUserIndex = 0;
-		await sleep(voteTime);
-		let leadingReaction = ['', 0];
-		victimSelectionMessage.reactions.cache.forEach((reaction) => {
-			if (reaction.count > leadingReaction[1]) {
-				leadingReaction = [reaction.emoji.name, reaction.count];
-			}
-		});
-
-		selectionReactions.forEach((emoji: string, i: number) => {
-			if (leadingReaction[0] == emoji) {
-				selectedUserIndex = i;
-				return;
-			}
-		});
-
-		return selectedUserIndex;
-	}
-
-	async checkWin() {
-		let werewolfCount = 0;
-		let villagerCount = 0;
-		for (const playerRole of this.playerRoles) {
-			if (playerRole[1] == Roles.Werewolf) {
-				werewolfCount++;
-			} else {
-				villagerCount++;
-			}
-		}
-
-		if (werewolfCount >= villagerCount) {
-			await this.endGame(true);
-		} else if (werewolfCount == 0) {
-			await this.endGame(false);
-		}
-	}
-
-	async endGame(hasWerewolvesWon: boolean) {
-		await this.werewolvesChannel.delete();
-		await this.generalChannel.delete();
-		await this.message.channel.send(`The game of werewolf has ended. The winners are the ${hasWerewolvesWon ? 'werewolves' : 'villagers'}!`);
-		this.gameRunning = false;
-	}
-
-	async killPlayer(player: User) {
-		await this.generalChannel.permissionOverwrites.edit(player.id, {SEND_MESSAGES: false});
-		await this.werewolvesChannel.permissionOverwrites.edit(player.id, {SEND_MESSAGES: false});
-		this.players.splice(this.players.indexOf(player));
-		await this.checkWin();
-	}
-
-	async sendDayMessages(victim: User) {
-		await this.generalChannel.send({embeds: [
-			new MessageEmbed()
-				.setTitle('Day Time:')
-				.setDescription(`As dawn breaks you discover the dead body of ${victim}. It is your job to decide who to kill`)
-				.setColor(EmbedColor),
-		]});
-		await this.werewolvesChannel.send({embeds: [
-			new MessageEmbed()
-				.setTitle('Day Time:')
-				.setDescription(`${victim} was successfully killed, and now you need to go to the ${this.generalChannel} to convince the villagers you are innocent`)
-				.setColor(EmbedColor),
-		]});
-	}
-
-	async sendNightMessages() {
-		await this.generalChannel.send({embeds: [
-			new MessageEmbed()
-				.setTitle('Night Time:')
-				.setDescription('The werewolves will now decide who to kill. If you have a special role you will be DMd when you need to make a choice')
-				.setColor(EmbedColor),
-		]});
-		await this.werewolvesChannel.send({embeds: [
-			new MessageEmbed()
-				.setTitle('Night Time:')
-				.setDescription('You need to choose who to kill. When you have chosen who to kill just mention them in an empty message')
-				.setColor(EmbedColor),
-		]});
 	}
 
 	async createChannels() {
-		const werewolvesPermissionOverride = [];
-		const generalPermissionsOverride = [];
-		werewolvesPermissionOverride.push({
-			id: this.interaction.guild.roles.everyone,
-			deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-		});
-		generalPermissionsOverride.push({
-			id: this.interaction.guild.roles.everyone,
-			deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-		});
-
-		for (const playerRole of this.playerRoles) {
-			if (playerRole[1] == Roles.Werewolf) {
-				werewolvesPermissionOverride.push({
-					id: playerRole[0].id,
-					allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-				});
-			}
-
-			generalPermissionsOverride.push({
-				id: playerRole[0].id,
-				allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-			});
-		}
-
-		this.generalChannel = await this.interaction.guild.channels.create('Werewolf - General', {
+		this.interaction.guild.channels.create('new-channel', {
 			type: 'GUILD_TEXT',
-			permissionOverwrites: generalPermissionsOverride,
+			permissionOverwrites: [
+				{
+					id: this.interaction.guild.roles.everyone,
+					deny: [Permissions.FLAGS.VIEW_CHANNEL],
+				},
+			],
 		});
-		this.werewolvesChannel = await this.interaction.guild.channels.create('Werewolf - Werewolves', {
-			type: 'GUILD_TEXT',
-			permissionOverwrites: werewolvesPermissionOverride,
-		});
-
-		await this.generalChannel.send({embeds: [
-			new MessageEmbed()
-				.setTitle('Werewolf')
-				.setDescription('Welcome to Werewolf. This is the channel for everyone, so keep your role here secret. If you have a special role like seer or doctor you will be ' +
-					'DMd. Werewolves have a private channel that villagers cannot see.')
-				.setColor(EmbedColor),
-		]});
-
-		await this.werewolvesChannel.send({embeds: [
-			new MessageEmbed()
-				.setTitle('Werewolf')
-				.setDescription('Welcome to werewolf. You are all werewolves and your goal is to kill all the villagers')
-				.setColor(EmbedColor),
-		]});
 	}
-}
-
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
 }
