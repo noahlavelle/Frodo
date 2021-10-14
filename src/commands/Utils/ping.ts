897688b2b36d1c3a681741ae04682eb5497e6ac4
Daniel Howard <danielnhoward93@gmail.com>
2021-10-14 16:54:28 +0100

    ping fix

--- src/commands/Utils/ping.ts
+++ src/commands/Utils/ping.ts
@@ -1,15 +1,10 @@
-import handleError, {getMessage} from '../../utils';
+import {getMessage} from '../../utils';
 import {client} from '../../index';
 import {webSocketPing, reconnecting} from '../../votes';
 
 export async function ping(interaction) {
 	const message = await getMessage(interaction);
-	await message.edit('`Pinging...`')
-		.catch((err) => {
-			handleError(err, interaction);
-		});
-	await message.edit(`Ping: \`${message.message.createdTimestamp - interaction.createdTimestamp}\`\nDiscord API ping: \`${client.ws.ping}\`\nWebSocket Ping: \`${reconnecting ? 'Disconnected' : webSocketPing}\``)
-		.catch((err) => {
-			handleError(err, interaction);
-		});
+	if (!message) return;
+	await message.edit('`Pinging...`');
+	await message.edit(`Ping: \`${message.message.createdTimestamp - interaction.createdTimestamp}\`\nDiscord API ping: \`${client.ws.ping}\`\nWebSocket Ping: \`${reconnecting ? 'Disconnected' : webSocketPing}\``);
 };
